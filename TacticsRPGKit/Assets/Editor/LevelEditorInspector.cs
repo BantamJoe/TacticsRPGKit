06810044a6fb4162be73ffbad35f2a46c331cad7
Malik Gray <mikey@starfantasygames.com>
2016-07-31 20:39:08 -0400

    Created a Level Editor and Struct to hold a Point.
    Created a Level Editor and Struct to hold a Point. Created a Scene to
    test the Level Editor.

---
+++ TacticsRPGKit/Assets/Editor/LevelEditorInspector.cs
@@ -0,0 +1,71 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(LevelEditor))]
+public class LevelEditorInspector : Editor
+{
+	public LevelEditor current {
+		get {
+			return (LevelEditor)target;
+		}
+	}
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Reset")) {
+			current.Reset();
+		}
+		if (GUILayout.Button("Grow")) {
+			current.Grow();
+		}
+		if (GUILayout.Button("Shrink")) {
+			current.Shrink();
+		}
+		if (GUILayout.Button("Grow Area")) {
+			current.GrowArea();
+		}
+		if (GUILayout.Button("Shrink Area")) {
+			current.ShrinkArea();
+		}
+		if (GUILayout.Button("Save")) {
+			current.Save();
+		}
+		if (GUILayout.Button("Load")) {
+			current.Load();
+		}
+		if (GUI.changed) {
+			current.UpdateMarker();
+		}
+	}
+
+	void OnSceneGUI()
+	{
+		Event e = Event.current;
+		switch(e.type)
+		{
+		case EventType.keyDown:
+			{
+				if(Event.current.keyCode == (KeyCode.A))
+				{
+					current.MoveMarkerLeft();
+				}
+				if(Event.current.keyCode == (KeyCode.D))
+				{
+					current.MoveMarkerRight();
+				}
+				if(Event.current.keyCode == (KeyCode.W))
+				{
+					current.MoveMarkerUp();
+				}
+				if(Event.current.keyCode == (KeyCode.S))
+				{
+					current.MoveMarkerDown();
+				}
+				break;
+			}
+		}
+	}
+}
---
+++ TacticsRPGKit/Assets/Scripts/Model/LevelData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelData : ScriptableObject
+{
+	public string LevelName;
+	public List<Vector3> Tiles;
+}
